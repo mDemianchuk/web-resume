3db3b099cf85a66fe995f8ba3f2e50f18f7e13d7
mdemianchuk <nickdemianchuk@gmail.com>
2021-10-24 00:01:30 -0500

    Configure CI/CD with TravisCI

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-resume/' : '/',
   transpileDependencies: ['vuetify'],
   configureWebpack: {
     resolve: {
