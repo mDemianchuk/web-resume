d27ed264b29c2c03120db5f946dc8387a4b5399e
mdemianchuk <nickdemianchuk@gmail.com>
2021-10-30 17:23:47 -0500

    Add degree and dates to the education section

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -16,9 +16,15 @@ export default {
   <ResumeSection :title="title">
     <v-row v-for="(school, i) in schoolList" :key="i">
       <v-col>
-        <strong>{{ school.name }}</strong>
-        <br />
-        <span>{{ school.text }}</span>
+        <v-row no-gutters align="baseline" class="mb-4">
+          <v-col cols="auto">
+            <h3 v-text="school.degree" class="colorText" />
+            <h4 v-text="school.name" />
+          </v-col>
+          <v-spacer />
+          <h4 v-text="school.dates" class="lightText" />
+        </v-row>
+        <p v-text="school.text" />
       </v-col>
     </v-row>
   </ResumeSection>
