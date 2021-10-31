d0be5db076dd04acb4b8f810b14fa0e094b50462
mdemianchuk <nickdemianchuk@gmail.com>
2021-10-31 12:02:43 -0500

    Refactor project name to a link

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -16,15 +16,16 @@ export default {
   <ResumeSection :title="title">
     <v-row v-for="(project, i) in projectList" :key="i">
       <v-col>
-        <v-row no-gutters align="baseline" class="mb-4">
+        <v-row no-gutters align="end" class="mb-4">
           <v-col cols="auto">
-            <h4 v-text="project.name" />
+            <h4>
+              <a v-if="project.url" :href="project.url">
+                <v-icon class="headingText" left small>mdi-link-variant</v-icon>
+                <span v-text="project.name" class="headingText" />
+              </a>
+              <span v-else v-text="project.name" class="headingText" />
+            </h4>
           </v-col>
-          <v-spacer />
-          <a :href="project.link.url">
-            <v-icon class="headingText" left small>mdi-link-variant</v-icon>
-            <span v-text="project.link.text" />
-          </a>
         </v-row>
         <p v-text="project.description" />
         <p v-text="project.footer" class="font-italic" />
