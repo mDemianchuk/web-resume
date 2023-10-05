5cb4e1139242a9716f7bfadda1f9e9e6fd6cea60
Nick Demianchuk <nickdemianchuk@gmail.com>
2023-10-05 10:15:06 -0500

    Cleanup

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,5 +1,6 @@
 locals {
-  caching_disabled_policy_id = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+  one_hour_in_seconds = 60 * 60
+  one_day_in_seconds  = local.one_hour_in_seconds * 24
 }
 
 resource "aws_cloudfront_origin_access_identity" "web_resume_app" {
@@ -15,14 +16,21 @@ resource "aws_cloudfront_distribution" "web_resume_app" {
     }
   }
   enabled             = true
-  is_ipv6_enabled     = true
   default_root_object = "${var.source_s3_prefix}/index.html"
   default_cache_behavior {
-    cache_policy_id        = local.caching_disabled_policy_id
-    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
-    cached_methods         = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id       = aws_s3_bucket.web_resume_app.id
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.web_resume_app.id
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
     viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = local.one_hour_in_seconds
+    max_ttl                = local.one_day_in_seconds
   }
   price_class = "PriceClass_100"
   restrictions {
