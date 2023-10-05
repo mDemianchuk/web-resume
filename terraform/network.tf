08574f5eacde2c86840dec50b5fdf5e56d876a22
Nick Demianchuk <nickdemianchuk@gmail.com>
2023-10-05 11:38:43 -0500

    Add CNAME record validation

--- terraform/network.tf
+++ terraform/network.tf
@@ -11,6 +11,23 @@ resource "aws_acm_certificate" "web_resume_app" {
   validation_method = "DNS"
 }
 
+resource "aws_route53_record" "web_resume_app" {
+  for_each = {
+    for option in aws_acm_certificate.web_resume_app.domain_validation_options : option.domain_name => {
+      name   = option.resource_record_name
+      record = option.resource_record_value
+      type   = option.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.web_resume_app.zone_id
+}
+
 resource "aws_acm_certificate_validation" "web_resume_app" {
   certificate_arn = aws_acm_certificate.web_resume_app.arn
 }
