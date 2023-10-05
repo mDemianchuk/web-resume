ebf6bf88a327344818265a5f05db7d461bb9cfe8
Nick Demianchuk <nickdemianchuk@gmail.com>
2023-10-05 10:39:41 -0500

    Add missing var

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,3 +1,7 @@
+locals {
+  caching_disabled_policy_id = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+}
+
 resource "aws_cloudfront_origin_access_identity" "web_resume_app" {
   comment = var.source_bucket_name
 }
