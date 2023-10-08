53cb22b5c5c34c92fcac5f36aa516092c92c4a1a
Nick Demianchuk <nickdemianchuk@gmail.com>
2023-10-08 15:20:53 -0500

    Add us_east_1 prodider

--- terraform/network.tf
+++ terraform/network.tf
@@ -14,11 +14,11 @@ data "aws_route53_zone" "web_resume_app" {
 }
 
 resource "aws_acm_certificate" "cert" {
-  count = local.has_custom_domain ? 1 : 0
+  provider = aws.us_east_1
+  count    = local.has_custom_domain ? 1 : 0
 
   domain_name       = local.app_domain_name
   validation_method = "DNS"
-  region            = "us-east-1"
 }
 
 resource "aws_route53_record" "cert_cname" {
@@ -39,10 +39,10 @@ resource "aws_route53_record" "cert_cname" {
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
-  count = local.has_custom_domain ? 1 : 0
+  provider = aws.us_east_1
+  count    = local.has_custom_domain ? 1 : 0
 
   certificate_arn = aws_acm_certificate.cert[0].arn
-  region          = "us-east-1"
 }
 
 resource "aws_cloudfront_origin_access_identity" "web_resume_app" {
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,3 +5,13 @@ provider "aws" {
     role_arn = var.deployment_role_arn
   }
 }
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "us_east_1"
+
+  assume_role {
+    role_arn = var.deployment_role_arn
+  }
+}
+
