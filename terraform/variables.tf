ce003b0b4b867b9ccdddae8762ee0a510ddd2a9a
Nick Demianchuk <nickdemianchuk@gmail.com>
2023-11-20 17:19:07 -0600

    Refactor `aws_s3_object` into a single resource definition (#22)
    * Refactor aws_s3_object into a single resource definition
    
    * Rename the resource
    
    * Fix file extension syntax
    
    * Move split index into the correct spot
    
    * Extract file extension using the element function
    
    * Can't spell length

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,3 +25,15 @@ variable "source_s3_prefix" {
   type    = string
   default = "web-resume"
 }
+
+variable "file_types" {
+  description = "Mapping of file types to their respective content types"
+  type        = map(string)
+  default     = {
+    js  = "text/javascript"
+    css = "text/css"
+    map = "application/json"
+    html = "text/html"
+    ico = "image/x-icon"
+  }
+}
