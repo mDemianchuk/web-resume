1418fffab7af869f8151630401aa937c7f9e5610
Nick Demianchuk <nickdemianchuk@gmail.com>
2023-11-20 17:44:10 -0600

    Add a TF variable for source_path

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,36 +5,42 @@ variable "region" {
 }
 
 variable "deployment_role_arn" {
-  description = "IAM Role ARN used to deploy the Terraform infrastructure"
   type        = string
+  description = "IAM Role ARN used to deploy the Terraform infrastructure"
 }
 
 variable "domain_name" {
-  description = "Optional domain name for the web app"
   type        = string
+  description = "Optional domain name for the web app"
   default     = ""
 }
 
 variable "subdomain_name" {
-  description = "Subdomain name for the web app (only applicable when domain_name is set)"
   type        = string
+  description = "Subdomain name for the web app (only applicable when domain_name is set)"
   default     = "resume"
 }
 
 variable "source_bucket_name" {
-  description = "S3 Bucket name for the web app build artifacts"
   type        = string
+  description = "S3 Bucket name for the web app build artifacts"
 }
 
 variable "source_s3_prefix" {
-  description = "S3 Bucket prefix that corresponds the base URL for the web app"
   type        = string
+  description = "S3 Bucket prefix that corresponds the base URL for the web app"
   default     = "web-resume"
 }
 
+variable "source_path" {
+  type        = string
+  description = "Source path to the build artifacts, relative to the root module"
+  default     = "dist"
+}
+
 variable "file_types" {
-  description = "Mapping of file types to their respective content types"
   type        = map(string)
+  description = "Mapping of file types to their respective content types"
   default = {
     js   = "text/javascript"
     css  = "text/css"
