21bff591ad52cb2fc82cc1ecad58a8ae7faab836
Nick Demianchuk <nickdemianchuk@gmail.com>
2023-11-20 17:12:13 -0600

    Extract file extension using the element function

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -49,5 +49,5 @@ resource "aws_s3_object" "source_files" {
   key          = "${var.source_s3_prefix}/${each.key}"
   source       = "${local.source_base_path}/${each.key}"
   etag         = filemd5("${local.source_base_path}/${each.key}")
-  content_type = var.file_types[split(".", basename(each.key))[1]]
+  content_type = var.file_types[element(split(".", basename(each.key)), lenght(split(".", basename(each.key))) - 1)]
 }
