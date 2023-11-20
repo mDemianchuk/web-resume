69f5c34937e26bbbc804fdf30a401357ba4f6381
Nick Demianchuk <nickdemianchuk@gmail.com>
2023-11-20 17:30:36 -0600

    Add missing TF variable descriptions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,39 +1,45 @@
 variable "region" {
-  type    = string
-  default = "us-east-1"
+  type        = string
+  description = "Target AWS Region"
+  default     = "us-east-1"
 }
 
 variable "deployment_role_arn" {
-  type = string
+  description = "IAM Role ARN used to deploy the infrastructure"
+  type        = string
 }
 
 variable "domain_name" {
-  type    = string
-  default = ""
+  description = "Optional domain name for the web app"
+  type        = string
+  default     = ""
 }
 
 variable "subdomain_name" {
-  type    = string
-  default = "resume"
+  description = "Subdomain name for the web app (only applicable when domain_name is set)"
+  type        = string
+  default     = "resume"
 }
 
 variable "source_bucket_name" {
-  type = string
+  description = "S3 Bucket name for the web app build artifacts"
+  type        = string
 }
 
 variable "source_s3_prefix" {
-  type    = string
-  default = "web-resume"
+  description = "S3 Bucket prefix that corresponds the base URL for the web app"
+  type        = string
+  default     = "web-resume"
 }
 
 variable "file_types" {
   description = "Mapping of file types to their respective content types"
   type        = map(string)
-  default     = {
-    js  = "text/javascript"
-    css = "text/css"
-    map = "application/json"
+  default = {
+    js   = "text/javascript"
+    css  = "text/css"
+    map  = "application/json"
     html = "text/html"
-    ico = "image/x-icon"
+    ico  = "image/x-icon"
   }
 }
